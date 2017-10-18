5d727d331dae259f43f95ac9206da0a1c74d35b9
George Sumpster <gsumpster@gmail.com>
2017-10-18 11:22:50 -0700

    feat(mobile): added mobile navigation menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
 document.getElementById('nav-toggle').onclick = function() {
-    document.getElementById('nav-toggle').classList.toggle('is-active')
-}
+    document.getElementById("nav-toggle").classList.toggle("is-active");
+    document.getElementById('nav').classList.toggle('nav--mobile');
+    document.getElementsByTagName('body')[0].classList.toggle('nav-open');
+};
